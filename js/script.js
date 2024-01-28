60390c1b0ad8bbc92f9e75a5d38b8673e11c9255
Regina Yashina <reginayashina@inbox.ru>
2024-01-28 20:27:32 +0300

    feat: add fixed header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,51 @@
+// header & fixed button
+const header = document.getElementById("header");
+const headerFix = document.getElementById("header-fix");
+const headerSimpleFix = document.getElementById("header-simple-fix");
+const fullscreen = document.querySelector('.fullscreen');
+const callbackButton = document.getElementById('callback-button');
+
+if (!!headerSimpleFix) {
+  const headerSimpleFixHeight = headerSimpleFix.offsetHeight;
+  window.addEventListener('scroll', function () {
+    let scrollDistance = window.scrollY;
+    if (scrollDistance >= headerSimpleFixHeight) {
+      headerSimpleFix.classList.add('sticky');
+
+    } else {
+      headerSimpleFix.classList.remove('sticky');
+
+    }
+  })
+}
+
+if (!!fullscreen) {
+  const fullscreenHeight = fullscreen.offsetHeight;
+  window.addEventListener('scroll', function () {
+    let scrollDistance = window.scrollY;
+    if (scrollDistance >= fullscreenHeight) {
+      header.classList.add('sticky');
+      headerFix.classList.add('sticky')
+      callbackButton.classList.add('sticky')
+    } else {
+      header.classList.remove('sticky');
+      headerFix.classList.remove('sticky')
+      callbackButton.classList.remove('sticky')
+    }
+  })
+}
+
+
+
+
 // burger
-let burger = document.querySelector(".burger");
-burger.addEventListener("click", function () {
-  burger.classList.toggle("is-active");
-});
+let burgers = document.querySelectorAll(".burger");
+burgers.forEach(function (burger) {
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("is-active");
+  });
+})
+
 
 // swiper
 new Swiper(".fullscreen-slider", {
@@ -114,3 +157,61 @@ infoButtons.forEach(function (item) {
     }
   })
 })
+
+// calculator
+
+// let checkboxes = document.querySelectorAll('.calculator-input');
+// checkboxes.forEach(function (item) {
+//   let checkedPrice;
+//   let finishPrice = 0;
+//   item.addEventListener('input', function () {
+//     let checked = document.querySelectorAll('.calculator-input:checked');
+//     // console.log(checked);
+//     for (let i = 0; i < checked.length; i++) {
+//       checkedPrice = Number(checked[i].closest('.calculator-block').querySelector('.calculator-cost span').innerText);
+//       finishPrice += checkedPrice;
+//       document.querySelector('.calculator-finish-price').innerText = finishPrice;
+//       console.log(finishPrice);
+//     }
+//   })
+// })
+
+// let checkboxes = document.querySelectorAll('.calculator-input');
+// checkboxes.forEach(function (item) {
+//   let checkedPrice = Number(item.closest('.calculator-block').querySelector('.calculator-cost span').innerText);
+//   let finishPrice = 0;
+//   item.addEventListener('input', function () {
+//     for (let i = 0; i < checkboxes.length; i++) {
+//       if (checkboxes[i].checked) {
+//         finishPrice += Number(checkboxes[i].closest('.calculator-block').querySelector('.calculator-cost span').innerText);
+//       } else {
+//         finishPrice = finishPrice - Number(checkboxes[i].closest('.calculator-block').querySelector('.calculator-cost span').innerText);
+//       }
+//       console.log(finishPrice);
+//     }
+
+//   })
+
+// })
+
+// to top button
+// const goTopBtn = document.querySelector(".go-top");
+// window.addEventListener("scroll", trackScroll);
+// goTopBtn.addEventListener("click", goTop);
+// function trackScroll() {
+//   const scrolled = window.scrollY;
+//   const coords = document.documentElement.clientHeight;
+//   if (scrolled > coords) {
+//     goTopBtn.classList.add("show");
+//   } else {
+//     goTopBtn.classList.remove("show");
+//   }
+// }
+
+// function goTop() {
+//   if (window.scrollY > 0) {
+//     window.scrollBy(0, -30); // второй аргумент - скорость
+//     setTimeout(goTop, 0); // входим в рекурсию
+//   }
+// }
+
